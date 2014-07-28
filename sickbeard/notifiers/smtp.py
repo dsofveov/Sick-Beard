397858d2b79f7dec933b8e908ca69f6d56c7f22b
Doug Forrest <doug@pastard.co.uk>
2014-07-28 21:26:25 +0100

    stuff

--- sickbeard/notifiers/smtp.py
+++ sickbeard/notifiers/smtp.py
@@ -40,7 +40,7 @@ class SMTPNotifier:
             self._notify(notifyStrings[NOTIFY_DOWNLOAD], ep_name)
 
     def test_notify(self, server_address, server_port, to_address, from_address=DEFAULT_SENDER):
-        return self._notify(server_address, server_port, "Test", "This is a test notification from Sick Beard", to_address)
+        return self._notify(server_address, server_port, "Test", "This is a test notification from Sick Beard", to_address, from_address)
 
     def update_library(self, ep_obj=None):
         pass
