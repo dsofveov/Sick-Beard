52582cc2c547eab84e7620554404a2bc99056c9e
Doug Forrest <doug@pastard.co.uk>
2014-07-29 18:31:47 +0100

    force = true

--- sickbeard/notifiers/smtp.py
+++ sickbeard/notifiers/smtp.py
@@ -39,8 +39,8 @@ class SMTPNotifier:
         if sickbeard.SMTP_NOTIFY_ONSNATCH:
             self._notify(notifyStrings[NOTIFY_DOWNLOAD], ep_name)
 
-    def test_notify(self, server_address, server_port, to_address, from_address=DEFAULT_SENDER):
-        return self._notify(server_address, server_port, "Test", "This is a test notification from Sick Beard", to_address, from_address)
+    def test_notify(self, server_address, server_port, to_address, from_address=DEFAULT_SENDER, force=True):
+        return self._notify(server_address, server_port, "Test", "This is a test notification from Sick Beard", to_address, from_address, force)
 
     def update_library(self, ep_obj=None):
         pass
