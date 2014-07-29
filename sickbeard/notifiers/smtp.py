1a3503296161d52a0a8fdaab66a3945dc51d44d9
Doug Forrest <doug@pastard.co.uk>
2014-07-29 20:16:07 +0100

    server port to int

--- sickbeard/notifiers/smtp.py
+++ sickbeard/notifiers/smtp.py
@@ -11,11 +11,14 @@ DEFAULT_SENDER = 'sickbeard@' + HOSTNAME
 class SMTPNotifier:
 
     def _send_email(self, server_address, server_port, subject, message, to_address, from_address=DEFAULT_SENDER):
-        email_server = smtplib.SMTP(server_address, server_port)
+        email_server = smtplib.SMTP(server_address, int(server_port))
         try:
             email_server.sendmail(from_address, to_address, message)
+            return True
+
         except:
             logger.log(u"SMTP: _send_email " +message+" to " + to_address + " via " + server_address + ":" +server_port + "fail", logger.INFO)
+            return False
 
 
 
@@ -23,8 +26,8 @@ class SMTPNotifier:
     def _notify(self, server_address, server_port, subject, message, to_address, from_address, force=False):
         if not sickbeard.USE_SMTP and not force:
             return False
-
-        return self._send_email(server_address, server_port, subject, message, to_address, from_address)
+        else:
+            return self._send_email(server_address, server_port, subject, message, to_address, from_address)
 
 #######################################################################
 # Public functions
@@ -40,7 +43,7 @@ class SMTPNotifier:
             self._notify(notifyStrings[NOTIFY_DOWNLOAD], ep_name)
 
     def test_notify(self, server_address, server_port, to_address, from_address=DEFAULT_SENDER, force=True):
-        return self._notify(server_address, server_port, "Test", "This is a test notification from Sick Beard", to_address, from_address, force)
+        return self._notify(server_address, server_port, "Test", "This is a test notification from Sick Beard", to_address, from_address, True)
 
     def update_library(self, ep_obj=None):
         pass
